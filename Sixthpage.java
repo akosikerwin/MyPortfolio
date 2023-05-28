abe004f1ac01408573a7635f6a57429b6bc4a04b
AKOSIKERWIN <111828131+akosikerwin@users.noreply.github.com>
2023-05-28 22:29:05 +0800

    Create Sixthpage.java

---
+++ Sixthpage.java
@@ -0,0 +1,63 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Sixthpage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sixthpage frame = new Sixthpage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Sixthpage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 450);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\TOSHIBA\\Pictures\\Saved Pictures\\6.6.png"));
+		lblNewLabel.setBounds(0, 0, 800, 450);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("BACK");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Secondpage Secondpage = new Secondpage();
+				Secondpage.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setFont(new Font("Rockwell", Font.BOLD, 12));
+		btnNewButton.setBounds(306, 363, 89, 23);
+		contentPane.add(btnNewButton);
+	}
+
+}
